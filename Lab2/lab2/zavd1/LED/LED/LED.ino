9a70f52d384c127aaaabbc76d4fbb76ba591b493
Vlad14ok228 <Vladislavkorni1@gmail.com>
2025-12-04 12:26:29 +0200

    edited code lab2 zavd1

--- Lab2/lab2/zavd1/LED/LED/LED.ino
+++ Lab2/lab2/zavd1/LED/LED/LED.ino
@@ -1,25 +1,12 @@
-int LDR_Val = 0;       // Змінна для значення фоторезистора
-int sensor = 34;       // ADC пін для фоторезистора (GPIO34)
-int led = 26;          // Пін для LED
+const int ledPin = 25; // GPIO25
 
 void setup() {
-  Serial.begin(9600);   // Швидкість Serial Monitor
-  pinMode(led, OUTPUT); // LED як вихід
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  LDR_Val = analogRead(sensor); // Зчитування значення LDR
-  Serial.print("LDR Output Value: ");
-  Serial.println(LDR_Val);
-
-  // Якщо темно (значення LDR близько до 4000)
-  if (LDR_Val > 3000) {  
-    digitalWrite(led, HIGH);  // LED горить
-  }
-  // Якщо яскраво (значення LDR близько до 0–200)
-  else if (LDR_Val < 1500) {  
-    digitalWrite(led, LOW);   // LED вимкнено
-  }
-
-  delay(200); // Оновлення кожні 0.2 секунди
+  digitalWrite(ledPin, HIGH); // LED ON
+  delay(1000);
+  digitalWrite(ledPin, LOW);  // LED OFF
+  delay(1000);
 }
